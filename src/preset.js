de0ededc40d07861789559b0d984954e300ef117
Enzo Innocenzi <enzo@innocenzi.dev>
2020-08-04 04:32:58 +0200

    refactor: edit package.json, composer.json and app

--- src/preset.js
+++ src/preset.js
@@ -5,4 +5,30 @@ module.exports = Preset.make('Laravel Inertia')
 	.apply('use-preset/laravel-tailwindcss')
 		.title('Install Tailwind CSS from its preset')
 		.with('--no-interaction')
-		.chain();
+		.chain()
+
+	.editJson('package.json')
+		.title('Add Node dependencies')
+		.merge({
+			'@babel/plugin-syntax-dynamic-import': '^7.2.0',
+			'@inertiajs/inertia': '^0.1',
+			'@inertiajs/inertia-vue': '^0.1',
+			'vue-template-compiler': '^2.6.10',
+			'vue-meta': '^2.3.1',
+			vue: '^2.5.17',
+		})
+		.chain()
+
+	.editJson('composer.json')
+		.merge({
+			require: {
+				'inertiajs/inertia-laravel': '^0.2'
+			}
+		})
+		.chain()
+
+	.edit('config/app.php')
+		.search(/App\\Providers\\RouteServiceProvider::class,/)
+			.addAfter('App\\Providers\\InertiaServiceProvider::class,')
+			.end()
+		.chain()
