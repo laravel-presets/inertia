09751a4278ec83206a974ae58ea68c0e360e7f6a
Enzo Innocenzi <enzo@innocenzi.dev>
2021-07-13 20:36:02 +0200

    refactor: update example Vite resolver

--- templates/default/resources/scripts/main.ts
+++ templates/default/resources/scripts/main.ts
@@ -5,9 +5,13 @@ import 'tailwindcss/tailwind.css'
 function withVite(pages: Record<string, any>, name: string) {
 	for (const path in pages) {
 		if (path.endsWith(`${name.replace('.', '/')}.vue`)) {
-			return pages[path]()
+			return typeof pages[path] === 'function' 
+				? pages[path]()
+				: pages[path]
 		}
 	}
+
+	throw new Error('Page not found: ' + name)
 }
 
 createInertiaApp({
