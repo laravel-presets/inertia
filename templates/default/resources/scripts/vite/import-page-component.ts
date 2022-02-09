2d063947edba04e0e81209a87d385deda250974b
Imam Susanto <abc.imams@gmail.com>
2022-02-09 19:28:49 +0700

    fix: replace `replace()` method with `replaceAll()` (#8)
    Using `replace()` instead of `replaceAll()` will only replace the first dot. What we need is to replace all dots with a slash instead.

--- templates/default/resources/scripts/vite/import-page-component.ts
+++ templates/default/resources/scripts/vite/import-page-component.ts
@@ -1,7 +1,7 @@
 export function importPageComponent(name: string, pages: Record<string, any>) {
 	// eslint-disable-next-line no-restricted-syntax
 	for (const path in pages) {
-		if (path.endsWith(`${name.replace('.', '/')}.vue`)) {
+		if (path.endsWith(`${name.replaceAll('.', '/')}.vue`)) {
 			return typeof pages[path] === 'function'
 				? pages[path]()
 				: pages[path]
