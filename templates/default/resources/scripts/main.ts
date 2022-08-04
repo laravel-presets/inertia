9abe34e0cb06d8317e5a58411379d08dd26b1c8a
Enzo Innocenzi <enzo@innocenzi.dev>
2022-08-04 14:29:20 +0200

    feat: use `vite-plugin-laravel`'s `resolvePageComponent`

--- templates/default/resources/scripts/main.ts
+++ templates/default/resources/scripts/main.ts
@@ -1,9 +1,9 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
-import { importPageComponent } from '@/scripts/vite/import-page-component'
+import { resolvePageComponent } from 'vite-plugin-laravel/inertia'
 
 createInertiaApp({
-	resolve: (name) => importPageComponent(name, import.meta.glob('../views/pages/**/*.vue')),
+	resolve: (name) => resolvePageComponent(name, import.meta.glob('../views/pages/**/*.vue')),
 	setup({ el, app, props, plugin }) {
 		createApp({ render: () => h(app, props) })
 			.use(plugin)
